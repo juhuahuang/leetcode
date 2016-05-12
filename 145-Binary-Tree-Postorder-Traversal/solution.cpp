89e50909d53d1805892e118ac639c0e54c2e727f
juhuahuang <hjh.1222@gmail.com>
2016-05-12 04:20:25 -0400

    [145. Binary Tree Postorder Traversal][Compile Error]committed by LeetCode Extension

---
+++ 145-Binary-Tree-Postorder-Traversal/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+       stack<TreeNode*> ss;
+       TreeNode* node = root;
+       TreeNode* last_visit;
+       TreeNode* peek;
+       while( node || !ss.empty()){
+           if( node){
+               ss.push(node);
+               node = node->left;
+           }
+           else{
+               peek = ss.top();
+               if( peek->right == NULL || peek->right == last_visit ){
+                   result.push_back(peek->val);
+                   ss.pop();
+                   lastvisit = peek;
+               }
+               else{
+                   node = peek->right;
+               }
+           }
+       }
+    }
+};
