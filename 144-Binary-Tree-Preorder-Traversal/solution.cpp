ad2fd3ec06abaf1be6b046493239b12df412f1b8
juhuahuang <hjh.1222@gmail.com>
2016-05-12 04:51:51 -0400

    [144. Binary Tree Preorder Traversal][Compile Error]committed by LeetCode Extension

---
+++ 144-Binary-Tree-Preorder-Traversal/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<int> ss;
+        TreeNode* node = root;
+        while( node ){
+            if(node){
+                result.push_back(node->val);
+                ss.push(node);
+                node = node->left;
+            }
+            else{
+                
+                node = ss.top()->right;
+                ss.pop();
+            }
+        }
+        return result;
+    }
+};
