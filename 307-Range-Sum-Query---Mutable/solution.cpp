2a251d79c7eab41821251ed8d7005b0a2efc63e4
juhuahuang <hjh.1222@gmail.com>
2016-05-20 05:48:17 -0400

    [307. Range Sum Query - Mutable][Accepted]committed by LeetCode Extension

--- 307-Range-Sum-Query---Mutable/solution.cpp
+++ 307-Range-Sum-Query---Mutable/solution.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sum;
     NumArray(vector<int> &nums) {
         sum.resize( nums.size()+1);
-        sum[0] = nums[0];
+        sum[0] = 0;
         for( int i = 0;i< nums.size();i++){
             sum[i+1] = nums[i] + sum[i];
         }
