8a7c190433ec40fb8b268dea2766bfbde0239e96
juhuahuang <hjh.1222@gmail.com>
2016-05-20 05:12:33 -0400

    [349. Intersection of Two Arrays][Compile Error]committed by LeetCode Extension

---
+++ 349-Intersection-of-Two-Arrays/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        unordered_set<int> numSet;
+        for( int n:nums1){
+            numSet.add(n);
+        }
+        for( int n:nums2){
+            if(numSet.find( n) != numSet.end()){
+                result.push_back(n);
+            }
+        }
+        return reuslt;
+    }
+};
