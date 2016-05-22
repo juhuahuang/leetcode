ceb4d59cb5c970e34c69a4c5de2ffaafc4c41fdb
juhuahuang <hjh.1222@gmail.com>
2016-05-22 03:18:40 -0400

    [350. Intersection of Two Arrays II][Accepted]committed by LeetCode Extension

---
+++ 350-Intersection-of-Two-Arrays-II/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+       if( nums1.size() > nums2.size()) return intersect( nums2,nums1);
+       vector<int> result;
+       unordered_map<int,int> numCount;
+       for( int n : nums1){
+           if( numCount.find(n) != numCount.end()) numCount[n]++;
+           else numCount[n] = 1;
+       }
+       for( int n:nums2){
+           if( numCount.find(n) != numCount.end() && numCount[n] >0) {
+               numCount[n]--;
+               result.push_back(n);
+            }
+        }
+    return result;
+        
+    }
+};
