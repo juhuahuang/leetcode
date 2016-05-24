0eae6a1cf6ffbcd67ce888e59dfc1a0b785f0e3f
juhuahuang <hjh.1222@gmail.com>
2016-05-23 21:11:46 -0400

    [342. Power of Four][Accepted]committed by LeetCode Extension

--- 342-Power-of-Four/solution.cpp
+++ 342-Power-of-Four/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        if( n == 1 ) return true;
-        if( n%4 != 0 || n == 0)return false;
-        return isPowerOfFour(n/4);
+        if( num == 1 ) return true;
+        if( num%4 != 0 || num == 0)return false;
+        return isPowerOfFour(num/4);
     }
 };
