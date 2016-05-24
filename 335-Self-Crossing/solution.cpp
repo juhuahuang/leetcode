d1c6c2d72d812d8f82fcf7ea7e0785759dbd4b64
juhuahuang <hjh.1222@gmail.com>
2016-05-23 21:50:52 -0400

    [335. Self Crossing][Runtime Error]committed by LeetCode Extension

--- 335-Self-Crossing/solution.cpp
+++ 335-Self-Crossing/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isSelfCrossing(vector<int>& x) {
-        int i = 2;
-        for( i;i<x.size()&&x[i] > x[i-2];i++);
+        int i ;
+        for( i=2;i<x.size()&&x[i] > x[i-2];i++);
         if( i > 2 && x[i] >= x[i-2] - i==3?0:x[i-4] ) x[i-1] -=x[i-3];
         for( i +=1;i<x.size() && x[i] < x[i-2];i++);
         return i<x.size();
