e96fcbb941dd4c310b1c39f9835822c7a753e262
juhuahuang <hjh.1222@gmail.com>
2016-06-08 03:10:09 -0400

    [210. Course Schedule II][Compile Error]committed by LeetCode Extension

---
+++ 210-Course-Schedule-II/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> result;
+        if( numCourses == 0) return result;
+        vector<vector<int> > graph(numCourses);
+        vector<int> inDegree(numCourses,0);
+        for( auto &p:prerequisites){
+            numCourse[p.second].push_back(p.first);
+            inDegree[p.first]++;
+        }
+        unordered_set<int> startNode;
+        for( int i = 0;i<numCourse;i++){
+            if(inDegree[i] == 0) startNode.insert(i);
+        }
+        
+        if( inDegree.empty()) return result;
+        while( !startNode.empty()){
+            int node = *(startNode.begin());
+            result.push_back(node);
+            startNode.erase(node);
+            for(int neighbor : graph[node]){
+                if(--inDegree[neighbor] == 0) startNode.insert(neighbor);
+            }
+        }
+        if( result.size() == numCourses) return result;
+        else return vector<int>();
+        
+
+    }
+};
