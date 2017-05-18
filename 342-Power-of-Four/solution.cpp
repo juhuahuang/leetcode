dbb03abfb2ab891d400786fb67acbcb2d85a26f3
Ju Hua Huang <juhuan@microsoft.com>
2017-05-18 22:28:27 +0800

    remove

--- 342-Power-of-Four/solution.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        if( num == 1 ) return true;
-        if( num%4 != 0 || num == 0)return false;
-        return isPowerOfFour(num/4);
-    }
-};
